5c04d21bc7b2e08b3e5a49a3126bd21fb286068f
Veles-Pan <barbic-vika@ya.ru>
2021-03-06 18:04:24 +0300

    fetch: content

---
+++ scripts/scroll.js
@@ -0,0 +1,41 @@
+const linksScroll = document.querySelectorAll('.scroll-to');
+
+const cards = document.querySelectorAll('.card');
+
+const lineScroll = document.querySelector('.scroll__position');
+
+const intro = document.querySelector('.intro').offsetTop;
+
+const cardFirst = document.querySelector('#card-1').offsetTop;
+const cardSecond = document.querySelector('#card-2').offsetTop;
+const cardThird = document.querySelector('#card-3').offsetTop;
+
+function handleButtonClick (cardID) {
+    const cardElement = document.querySelector(`#${cardID}`);
+    cardElement.scrollIntoView({block: "center", behavior: "smooth"});
+}
+
+function scrollLine (scrollY) {
+    if (scrollY <= intro + 500) {
+        lineScroll.style.transform = `matrix(1, 0, 0, 1, 0, 0)`;
+      } else if (scrollY <= cardFirst + 500) {
+        lineScroll.style.transform = `matrix(1, 0, 0, 1, 0, 60)`;
+      } else if (scrollY <= cardSecond + 500) {
+        lineScroll.style.transform = `matrix(1, 0, 0, 1, 0, 120)`;
+    } else if (scrollY <= cardThird + 500) {
+        lineScroll.style.transform = `matrix(1, 0, 0, 1, 0, 180)`;
+    }
+}
+
+
+linksScroll.forEach(element => {
+    element.addEventListener('click', function() {
+        const cardID = element.id.slice(3);
+        handleButtonClick(cardID);
+    })
+});
+
+document.addEventListener('scroll', function() {
+    let scrollY = window.pageYOffset;
+    scrollLine(scrollY);
+})
