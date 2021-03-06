07427437a633ccda89542627cad6e807bb622427
Veles-Pan <barbic-vika@ya.ru>
2021-03-06 19:25:14 +0300

    fetch: footer and БЭМ

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -10,20 +10,31 @@ const cardFirst = document.querySelector('#card-1').offsetTop;
 const cardSecond = document.querySelector('#card-2').offsetTop;
 const cardThird = document.querySelector('#card-3').offsetTop;
 
+const socialLinks = document.querySelector('.social');
+const scrollMenu = document.querySelector('.scroll');
+
 function handleButtonClick (cardID) {
     const cardElement = document.querySelector(`#${cardID}`);
     cardElement.scrollIntoView({block: "center", behavior: "smooth"});
 }
 
 function scrollLine (scrollY) {
-    if (scrollY <= intro + 500) {
-        lineScroll.style.transform = `matrix(1, 0, 0, 1, 0, 0)`;
+    if (scrollY <= intro + 700) {
+        lineScroll.style.transform = 'matrix(1, 0, 0, 1, 0, 0)';
+        socialLinks.style.transform = 'matrix(1, 0, 0, 1, 0, 0)';
+        scrollMenu.style.transform = 'matrix(1, 0, 0, 1, 0, 0)';
       } else if (scrollY <= cardFirst + 500) {
-        lineScroll.style.transform = `matrix(1, 0, 0, 1, 0, 60)`;
+        lineScroll.style.transform = 'matrix(1, 0, 0, 1, 0, 60)';
+
       } else if (scrollY <= cardSecond + 500) {
-        lineScroll.style.transform = `matrix(1, 0, 0, 1, 0, 120)`;
+        lineScroll.style.transform = 'matrix(1, 0, 0, 1, 0, 120)';
     } else if (scrollY <= cardThird + 500) {
-        lineScroll.style.transform = `matrix(1, 0, 0, 1, 0, 180)`;
+        lineScroll.style.transform = 'matrix(1, 0, 0, 1, 0, 180)';
+    }
+
+    if (scrollY >= cardFirst - 600) {
+        socialLinks.style.transform = 'matrix(1, 0, 0, 1, 0, -280)';
+        scrollMenu.style.transform = 'matrix(1, 0, 0, 1, 0, -280)';
     }
 }
 
